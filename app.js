ccd44faceb3ca332128f97b136d269c60775543f
Milan Matejic <milanmatejicdev@gmail.com>
2021-07-27 19:47:31 +0200

    Added functionality to the app

---
+++ app.js
@@ -0,0 +1,9 @@
+// Selecting elements
+const btn = document.querySelector('.btn');
+const body = document.querySelector('body');
+const colors = ['green', 'yellow', 'purple', 'pink'];
+
+btn.addEventListener('click',  function changeColor() {
+    const colorIndex = parseInt(Math.random() * colors.length+1)
+    body.style.backgroundColor = colors[colorIndex];
+});
